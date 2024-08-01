ee74aeb3670fec80b2a390a2f6f5f4d85e59e202
Shantanu Singh <singhshantanu521@gmail.com>
2024-08-01 03:39:10 +0000

    new

--- first.py
+++ first.py
@@ -3,5 +3,18 @@ import numpy as np
 # print(arr)
 # print(np.flip(arr))
 
-arr1=np.array([[1,2,3],[2,4,5],[1,2,3]])
-print(arr1.flatten())
+# arr1=np.array([[1,2,3],[2,4,5],[1,2,3]])
+# print(arr1.flatten())
+
+# a1d=np.array([1,2,3,4])
+# a2d=np.array([[1,2],[3,4]])
+# a3d=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+
+# print(a3d.size)
+# print(a3d.ndim)
+
+# arr=np.arange(2,10,2,dtype=float)
+# print(arr)
+# b=np.linspace(1.,4.,6)
+# print(b)
+
